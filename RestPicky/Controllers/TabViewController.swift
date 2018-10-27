1d849ef4b094f1d8a2bf150f2881c8485fcd3528
nghia <tom.vuong008@gmail.com>
2018-10-27 01:39:54 -0500

    Update RestPicky/Controllers/TabViewController.swift

--- RestPicky/Controllers/TabViewController.swift
+++ RestPicky/Controllers/TabViewController.swift
@@ -10,24 +10,34 @@ import UIKit
 import Firebase
 
 class TabViewController: UITabBarController {
-
+    
+    var user = User()
+    var currentUser : String?
     var ref : DatabaseReference?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         ref = Database.database().reference()
+        user.uid = Auth.auth().currentUser?.uid ?? ""
+        ref?.child("user").child(user.uid).observeSingleEvent(of: .value) { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            self.user.name = value?["name"] as? String ?? ""
+            self.user.email = value?["email"] as? String ?? ""
+        }
+        
+        print(user.email)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
