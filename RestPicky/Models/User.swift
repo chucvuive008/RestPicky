a73c02c38b54ff716b28be82d748bc413c67694e
nghia <tom.vuong008@gmail.com>
2018-10-27 00:58:43 -0500

    add restaurant and user model

---
+++ RestPicky/Models/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  RestPicky
+//
+//  Created by Nghia Vuong on 10/25/18.
+//  Copyright © 2018 Nghia Vuong. All rights reserved.
+//
+
+import Foundation
+
+public class User {
+    var name : String
+    var uid : String
+    var email : String
+    
+    init() {
+        name = ""
+        uid = ""
+        email = ""
+    }
+}
