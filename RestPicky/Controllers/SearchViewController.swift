b8b7432cf8ce7604e1018c4e89512c1c38dd299f
nghia <tom.vuong008@gmail.com>
2018-10-27 01:40:49 -0500

    Update RestPicky/Controllers/SearchViewController.swift

--- RestPicky/Controllers/SearchViewController.swift
+++ RestPicky/Controllers/SearchViewController.swift
@@ -8,23 +8,54 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let restaurantCollections = ["New Restaurants", "Top Restaurants", "Random Restaurants", "Most review"]
+    
+    @IBOutlet weak var CollectionTableView: UITableView!
+    
+    @IBOutlet weak var searchField: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        searchField.backgroundImage = UIImage()
+        searchField.layer.borderWidth = 0
+        CollectionTableView.delegate = self
+        CollectionTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantCollections.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 180
+    }
+    
+    @IBAction func SeafoodButtonPress(_ sender: Any) {
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let collectionName = cell.viewWithTag(1) as! UILabel
+        collectionName.text = restaurantCollections[indexPath.row]
+        
+        return cell
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
