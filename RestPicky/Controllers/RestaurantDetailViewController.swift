803478f7f83cfe5a3d8a6d6bf63586b671b4ee2e
denysponce <denysonce@gmail.com>
2018-10-31 00:14:09 -0500

    Added Details: Image, rating, address

--- RestPicky/Controllers/RestaurantDetailViewController.swift
+++ RestPicky/Controllers/RestaurantDetailViewController.swift
@@ -10,23 +10,64 @@ import UIKit
 
 class RestaurantDetailViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
     var restaurant = Restaurant()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text = restaurant.name
+        Street.text = restaurant.street + "123 Main St." 
+        cityStateZip.text = restaurant.city + ", " + restaurant.state + " \(restaurant.zipcode)"
+        restaurantImage.image = restaurant.images[0]
         
+        paintStars()
         
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var star1: UIImageView!
+    @IBOutlet weak var star2: UIImageView!
+    @IBOutlet weak var star3: UIImageView!
+    @IBOutlet weak var star4: UIImageView!
+    @IBOutlet weak var star5: UIImageView!
+    
     @IBAction func backBtnPress(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var restaurantImage: UIImageView!
+    
+    @IBOutlet weak var Street: UILabel!
+    
+    @IBOutlet weak var cityStateZip: UILabel!
+    
+    func paintStars()
+    {
+        star1.image = UIImage(named: "YellowStarIcon")
+        star2.image = UIImage(named: "YellowStarIcon")
+        star3.image = UIImage(named: "YellowStarIcon")
+        star4.image = UIImage(named: "YellowStarIcon")
+        star5.image = UIImage(named: "YellowStarIcon")
+        var raiting = restaurant.rating
+        if raiting < 4.5{
+            star5.image = UIImage(named: "BlankStarIcon")
+        }
+        if raiting < 3.5{
+            star4.image = UIImage(named: "BlankStarIcon")
+        }
+        if raiting < 2.5{
+            star3.image = UIImage(named: "BlankStarIcon")
+        }
+        if raiting < 1.5{
+            star2.image = UIImage(named: "BlankStarIcon")
+        }
+        if raiting < 0.5{
+            star1.image = UIImage(named: "BlankStarIcon")
+        }
+        
+    }
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
