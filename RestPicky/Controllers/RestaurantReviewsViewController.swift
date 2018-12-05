9bed8cbd13aa438c8731612d3d5f4b0b9b1f84db
nghia <tom.vuong008@gmail.com>
2018-12-04 21:03:15 -0600

    review

--- RestPicky/Controllers/RestaurantReviewsViewController.swift
+++ RestPicky/Controllers/RestaurantReviewsViewController.swift
@@ -73,7 +73,12 @@ class RestaurantReviewsViewController: UIViewController {
         let reviewId = getReviewId()
         
         self.ref?.child("restaurant/\(restaurant.id)/review/\(reviewId)").setValue(["comment" : myReviewBox.text, "id" : reviewId, "rating" : myRating, "userId" : user.uid])
-        
+        var review = Review()
+        review.id = reviewId
+        review.comment = myReviewBox.text
+        review.rating = myRating
+        review.userId = user.uid
+        restaurant.review.append(review)
         ref?.child("Restaurant/\(self.restaurant.id)").observeSingleEvent(of: .value, with: { (snapshot) in
             self.restaurant = snapshot.value as? Restaurant ?? Restaurant()
         })
