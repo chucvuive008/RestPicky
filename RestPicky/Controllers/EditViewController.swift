005a0557299a5199260d87a3c729814c06b3412a
NickVu <nicvuki@gmail.com>
2018-12-05 19:49:57 -0600

    update

--- RestPicky/Controllers/EditViewController.swift
+++ RestPicky/Controllers/EditViewController.swift
@@ -11,15 +11,22 @@ import Firebase
 class EditViewController: UIViewController {
     var databaseRef : DatabaseReference!
     var userID: String = ""
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
-    
+
     @IBAction func submitButton(_ sender: Any) {
         //If text field is not empty update database with values entered
-        if(nameTextField.text != ""){postProfileInfo(type: "name", text: nameTextField.text!)}
-        if(addressTextField.text != ""){postProfileInfo(type: "address", text: addressTextField.text!)}
-        if(phoneTextField.text != ""){postProfileInfo(type: "phone", text: phoneTextField.text!)}
+//        if(nameTextField.text != ""){postProfileInfo(type: "name", text: nameTextField.text!)}
+//        if(addressTextField.text != ""){postProfileInfo(type: "address", text: addressTextField.text!)}
+//        if(phoneTextField.text != ""){postProfileInfo(type: "phone", text: phoneTextField.text!)}
+        let alert = UIAlertController(title: "Confirmation", message: "Do you want to confirm these changes?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action: UIAlertAction!) in print("Confirming")
+            }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { (action: UIAlertAction!) in print("Cancelling")
+            }))
+        self.present(alert, animated: true, completion: nil)
     }
     
     func postProfileInfo(type:String, text:String) {
@@ -42,8 +49,14 @@ class EditViewController: UIViewController {
             self.nameTextField.text = username
             self.addressTextField.text = address
             self.phoneTextField.text = phone
+            
         }) { (error) in
             print(error.localizedDescription)
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toProfile" {
+            let seguetoProf = segue.destination as! ProfileViewController
         }
     }
 }
